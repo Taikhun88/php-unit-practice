e8048ab2d0ef86b53ee5afc2a03942f550542d93
Taikhun <taiyang78190@gmail.com>
2022-04-16 18:30:24 +0200

    create new method tearDown --DONE

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -7,14 +7,39 @@ use PHPUnit\Framework\TestCase;
 
 class CartTest extends TestCase 
 {
+    protected $cart;
+
+    protected function setUp():void
+    {
+        Cart::setTax(1.2);
+        $this->cart = new Cart;
+    }
+
+    protected function tearDown(): void
+    {
+        // Cart::setTax(1.2);
+    }
+
+    public function testTheCartTaxValueCanBeChangedStatically()
+    {
+        // Setup
+        $this->cart->price = 10;
+
+        // Do something
+        Cart::setTax(1.5);
+        $netPrice = $this->cart->getNetPrice();
+        
+        // Make assertions
+        $this->assertEquals(15, $netPrice);
+    }
+
     public function testNetPriceIsCalculatedCorrectly()
     {
         // Setup or Giver : is the phase where you bring the dependencies to run the execution
-        $cart = new Cart();
-        $cart->price = 10;
+        $this->cart->price = 10;
 
         // Act or When : is the phase that will execute the thing or Do something 
-        $netPrice = $cart->getNetPrice();
+        $netPrice = $this->cart->getNetPrice();
         
         // Make or Then : assertions refers to assert or Then. These 3 phases are referred as (Given, When, Then)
         $this->assertEquals(12, $netPrice);
